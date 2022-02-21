571b188db38ba81ac8225aa8d76d738e7a87f6e4
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-02-21 16:25:45 +0530

    Binary search approach: ceiling and floor example

---
+++ Array/ceiling_and_floor.java
@@ -0,0 +1,65 @@
+// Binary search approach
+// Ceiling : Smallest element which is greater than or equal to target element
+// floor : Largest element which is smaller than or equal to target element
+
+/* Example
+Input : arr[] = 2, 3, 5, 9, 14, 16, 18, target = 15
+Output [array index] : Ceiling = 5 and floor = 4
+
+Input : arr[] = 2, 3, 5, 9, 14, 16, 18, target = 5
+Output [array index]: Ceiling = 2 and floor = 2 
+*/
+
+public class Main
+{
+    public static int ceiling(int arr[], int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if (arr[mid]==target)
+                return mid;
+            else if (target > arr[mid])
+                start = mid+1;
+            else 
+                end = mid-1;
+        }
+        return start;
+    }
+    
+    public static int floor(int arr[], int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if (arr[mid]==target)
+                return mid;
+            else if (target > arr[mid])
+                start = mid+1;
+            else 
+                end = mid-1;
+            
+        }
+        return end;
+    }
+    
+	public static void main(String[] args) {
+	    int arr[] = {2, 3, 5, 9, 14, 16, 18};
+	    int target = 15;
+	    
+	    int ans_ceiling = ceiling(arr, target);
+	    int ans_floor = floor(arr, target);
+	    
+	    System.out.println("Ceiling of a " + target + " is = " + ans_ceiling);
+	    System.out.println("Floor of a " + target + " is = " + ans_floor);
+	}
+}

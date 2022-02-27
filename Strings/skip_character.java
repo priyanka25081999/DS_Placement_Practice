e236a5903eea8b4caaf22ad7017d643b32bfab25
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-02-27 19:50:03 +0530

    added the code for skip the character

---
+++ Strings/skip_character.java
@@ -0,0 +1,28 @@
+// Skip the specific character from string
+
+public class Main
+{
+	public static void main(String[] args) {
+		skip("", "bcaccad");
+	}
+	
+	static void skip(String p, String up)
+	{
+	    if(up.isEmpty())
+	    {
+	        System.out.println("All done...Processed string is : "+ p);
+	        return;
+	    }
+	    
+	    char ch = up.charAt(0);
+      // skip 'a' from string
+	    if(ch=='a')
+	    {
+	        skip(p, up.substring(1));
+	    }
+	    else
+	    {
+	        skip(p+ch, up.substring(1));
+	    }
+	}
+}

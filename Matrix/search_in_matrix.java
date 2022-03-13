24ba1b4e27465bd56e56f2d5495279e3cf70de96
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-03-13 11:52:19 +0530

    Matrix : search element in 2D matrix

---
+++ Matrix/search_in_matrix.java
@@ -0,0 +1,27 @@
+// Matrix : Search an element in a 2D matrix
+// https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                // if an element equals to target element, return true
+                if(matrix[i][j] == target)
+                    return true;
+                // if an element lesser than target, keep searching in same row
+                else if(matrix[i][j] < target)
+                    continue;
+                // if an element larger than target, break the loop and move to next row.
+                else 
+                    break;     
+            }
+        }
+        return false;
+    }
+}

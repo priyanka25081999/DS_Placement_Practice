5eb112c90c3268411b05826fd3e275045c428f79
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-03-29 09:07:18 +0530

    Array : combinations - recursion and backtracking approach

--- Array/combinations.java
+++ Array/combinations.java
@@ -1,4 +1,5 @@
-// Array : Combinations
+// Array : Combinations 
+// Recursion and backtracking approach
 // Given two integers n and k, return all possible combinations of k numbers out of the range [1, n]. You may return the answer in any order.
 // https://leetcode.com/problems/combinations/
 

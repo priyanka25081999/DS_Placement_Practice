6198a162cf0cf2b6d40e93cc47a9765683be2565
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-03-31 08:50:09 +0530

    bit manipulation : power of two - recursion

---
+++ Bit Manipulation/power_of_two.java
@@ -0,0 +1,13 @@
+// Bit Manipulation : Power of Two
+// https://leetcode.com/problems/power-of-two/
+// Recursive approach
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n==1)
+            return true;
+        
+        return n>0 && (n%2==0 && isPowerOfTwo(n/2));
+    }
+}

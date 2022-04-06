7d698e5a9d16fd0c8f66034c5ed677a428abf3cc
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-04-06 08:47:47 +0530

    added hashmap approach

--- Array/3Sum_with_multiplicity.java
+++ Array/3Sum_with_multiplicity.java
@@ -1,7 +1,35 @@
 // Array : 3Sum with multiplicity 
-// Brute force approach (Getting TLE) - WIP
 // https://leetcode.com/problems/3sum-with-multiplicity/
 
+// HashMap approach
+class Solution {
+    public int threeSumMulti(int[] arr, int target) {
+        HashMap<Integer, Integer> hm=new HashMap<>();
+        int mod=(int)(1e9+7);
+        hm.put(arr[0], 1);
+        int ans=0;
+        for(int i=1;i<arr.length;i++) {
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[i]+arr[j]>target) 
+                    continue;
+                else {
+                    int re=target-(arr[i]+arr[j]);
+                    if(hm.containsKey(re)) 
+                        ans=(ans%mod+hm.get(re)%mod)%mod;
+                }
+            }
+            if(hm.containsKey(arr[i])) 
+                hm.put(arr[i], hm.get(arr[i])+1);
+            
+            else 
+                hm.put(arr[i], 1);
+        }
+        return ans;
+    }
+}
+
+// Brute force approach (Getting TLE) - WIP
+/*
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
         int count=0;
@@ -25,3 +53,4 @@ class Solution {
         return count;
     }
 }
+*/

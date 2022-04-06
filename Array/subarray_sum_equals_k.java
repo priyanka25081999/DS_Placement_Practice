259c5097c3bc748abeb34b65be045dfea749a675
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-04-06 08:32:54 +0530

    Array : Subarray sum equals k - hashmap approach

---
+++ Array/subarray_sum_equals_k.java
@@ -0,0 +1,25 @@
+// Array : Subarray sum equals K
+// HashMap approach
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        
+        int current_sum = 0, total_count = 0;
+        hmap.put(0, 1);
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            // calculate the current sum 
+            current_sum = current_sum + nums[i];
+            
+            if(hmap.containsKey(current_sum - k))
+                total_count = total_count + hmap.get(current_sum-k);
+            
+            hmap.put(current_sum, hmap.getOrDefault(current_sum, 0)+1);
+        }
+        
+        return total_count;
+    }
+}

e8dde8059f7e4e0b831781e46fa4b2a8ca30740a
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-04-10 16:45:46 +0530

    String : added another approach to reverse the string

--- Strings/reverse_string.java
+++ Strings/reverse_string.java
@@ -17,3 +17,26 @@ class Solution {
         }
     }
 }
+
+
+/*
+
+class Solution {
+    public void reverseString(char[] s) {
+        int j=s.length-1;
+        for(int i=0; i<j; i++)
+        {
+            if(i!=j)
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;   
+                j--;
+            }
+            else
+                break;
+        }
+    }
+}
+
+*/

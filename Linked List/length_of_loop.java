e7baf44c81478b10cbead21fc7a7bc88fe5a4875
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-04-16 11:40:55 +0530

    LinkedList : find length of a loop

---
+++ Linked List/length_of_loop.java
@@ -0,0 +1,32 @@
+// LinkedList : Find the length of loop
+// Two pointers solution
+// GFG : https://practice.geeksforgeeks.org/problems/find-length-of-loop/1/?page=2&category[]=Linked%20List&sortBy=submissions#
+
+class Solution
+{
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        //Add your code here.
+        
+        Node fast = head, slow = head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            // It's a loop condition
+            if(fast == slow)
+            {
+                int len = 1;
+                while(slow.next!=fast)
+                {   slow = slow.next;
+                    len++;
+                }
+                return len;
+            }
+        }
+        // if no loop, return 0
+        return 0;
+    }
+}

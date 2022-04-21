e260082894b6d13835d86c7daa2f90cc43828bb7
Priyanka Salunke <49332725+priyanka25081999@users.noreply.github.com>
2022-04-21 08:34:45 +0530

    added the code to design hashset

---
+++ Misc./design_hashset.java
@@ -0,0 +1,30 @@
+// HashSet : Design Hashset
+// https://leetcode.com/problems/design-hashset/
+
+class MyHashSet {
+
+    HashSet<Integer> hset;
+    public MyHashSet() {
+        hset = new HashSet<Integer>();
+    }
+    
+    public void add(int key) {
+        hset.add(key);
+    }
+    
+    public void remove(int key) {
+        hset.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return hset.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

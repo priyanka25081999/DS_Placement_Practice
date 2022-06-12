73734a4096777f23f4bdbc54fbf6eecee8d017b8
Priyanka Salunke <salunkeps99@gmail.com>
2022-06-12 09:36:45 +0530

    Array : contains duplicate

---
+++ cheatsheet/contains_duplicate.java
@@ -0,0 +1,38 @@
+// Array : Contains Duplicate
+// Approach-1 : Linear method 
+// Time complexity - O(nlogn) (performed sorting, assumed merge/quick sort)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        for(int i=1; i<nums.length; i++)
+        {
+            if(nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+    }
+}
+
+// Approach-2 : HashMap
+// Time complexity - O(n) (traverse the array once)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            hmap.put(nums[i], hmap.getOrDefault(nums[i],0)+1);
+        }
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            int cnt = hmap.get(nums[i]);
+            
+            if(cnt > 1)
+                return true;
+        }
+        return false;
+    }
+}

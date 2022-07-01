f682f8fac2ce927f31e7e84558e7636df96662e6
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-01 09:04:21 +0530

    Linked list cycle

---
+++ cheatsheet/linked_list_cycle.java
@@ -0,0 +1,30 @@
+// Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head, fast=head;
+        
+        while(fast!=null && fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(slow == fast)
+                return true;
+        }
+        
+        return false;
+    }
+}

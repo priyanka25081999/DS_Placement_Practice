bfdf590724d7434397115b1dfd87888bf53e6683
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-17 18:25:03 +0530

    String : added recursion approach

--- Misc./1_letter_combo_phone_no.java
+++ Misc./1_letter_combo_phone_no.java
@@ -40,3 +40,40 @@ class Solution {
         return res_list;
     }
 }
+
+// Another approach : Recursion
+/*
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String digits = "23";
+	    List<String> result = new ArrayList<>();
+        if(digits == null || digits.isEmpty())
+            System.out.println("res : "+ result);
+        Map<Character,String> map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        
+        helper(digits, map, 0, result, "");
+        return result;
+    }
+    public static void helper(String digits, Map<Character,String> map,int index, List<String> result, String str) {
+        if(str.length() == digits.length()) {
+            result.add(str);
+            return;
+        }
+
+        String chars = map.get(digits.charAt(index));
+        for(int i=0; i<chars.length(); i++) {
+            helper(digits, map, index+1, result, str+chars.charAt(i));
+        }
+    }
+}
+
+*/
+

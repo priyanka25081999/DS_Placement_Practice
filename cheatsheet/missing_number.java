a9520bfb7550a2dccaf10c9d58b7ef2027fcdfbd
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-29 08:43:55 +0530

    Array : missing number - added approach2

--- cheatsheet/missing_number.java
+++ cheatsheet/missing_number.java
@@ -16,3 +16,20 @@ class Solution {
         return i;
     }
 }
+
+// Approach2 : Time complexity O(n)
+class Solution {
+    public int missingNumber(int[] nums) { 
+        int total_cnt=0, current_cnt=0;
+        
+        // find the total count
+        for(int i=0; i<=nums.length; i++)
+            total_cnt = total_cnt + i;
+        
+        // find the current count
+        for(int i=0; i<nums.length; i++)
+            current_cnt = current_cnt + nums[i];
+        
+        return total_cnt - current_cnt;
+    }
+}

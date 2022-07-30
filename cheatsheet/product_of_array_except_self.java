569bb2c223f8b402f57f75bc41bffffa2ea893fd
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-30 18:37:45 +0530

    Array : added division operator approach

--- cheatsheet/product_of_array_except_self.java
+++ cheatsheet/product_of_array_except_self.java
@@ -24,3 +24,17 @@ class Solution {
         return ans;
     }
 }
+
+// Second cool approach is by using division operator
+// Take the product of whole input array and divide it by position wise
+// For example: nums[] = {1,2,3,4}
+// Product of nums array, prod = 24
+// Create an answer array and put the product by following
+// ans[0] = prod/nums[i]   (where, i = 0, and nums[i] = 1. Hence ans[0] = 24)
+// ans[1] = prod/nums[i]   (where, i = 1, and nums[i] = 2. Hence ans[1] = 12)
+// ans[2] = prod/nums[i]   (where, i = 2, and nums[i] = 3. Hence ans[2] = 8)
+// ans[3] = prod/nums[i]   (where, i = 3, and nums[i] = 4. Hence ans[3] = 6)
+// But, due to this limitation ("You must write an algorithm that runs in O(n) time and without using the division operation.") given in the problem
+// we will not use division operator
+
+

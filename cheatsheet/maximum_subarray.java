fcea6e5e7e362d11f6328997e411a25b2a5d92c3
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-31 09:58:17 +0530

    Array : added more description

--- cheatsheet/maximum_subarray.java
+++ cheatsheet/maximum_subarray.java
@@ -1,6 +1,10 @@
 // Array : Maximum subarray (Kadane's algorithm)
 // Leetcode : https://leetcode.com/problems/maximum-subarray/
+// Link for reference : https://www.youtube.com/watch?v=5WZl3MMT0Eg
 // Approach-1 : Kadane's Algorithm O(n)
+// Linear algorithm and no extra memory required
+// We keep increamenting our right pointer and ignore the -ve elements of the array
+// We remove the -ve prefix of the array
 
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -8,10 +12,12 @@ class Solution {
         if(nums.length < 2)
             return nums[0];
         
+        // current max should contain 1st array element, by assuming it as our first max sum.
         int current_sum = 0, current_max = nums[0];
         
         for(int i=0; i<nums.length; i++)
         {
+            // store the subarray sum into current_sum and add new element in it (DP)
             current_sum = Math.max(current_sum+nums[i], nums[i]);
             current_max = Math.max(current_max, current_sum);
         }

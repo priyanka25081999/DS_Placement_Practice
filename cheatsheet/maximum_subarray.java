fb4a353a9e66b162db9fae6d15ae9245ae54ba5a
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-31 09:48:55 +0530

    Array : added approach 2

--- cheatsheet/maximum_subarray.java
+++ cheatsheet/maximum_subarray.java
@@ -1,5 +1,6 @@
 // Array : Maximum subarray (Kadane's algorithm)
 // Leetcode : https://leetcode.com/problems/maximum-subarray/
+// Approach-1 : Kadane's Algorithm O(n)
 
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -19,3 +20,24 @@ class Solution {
     }
 }
 
+// Approach-2 : Brute force method - O(n^2)
+// Getting TLE (Time Limit Exceeded)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int i=0, j=0, max_sum = Integer.MIN_VALUE;
+        
+        for(i=0; i<nums.length; i++)
+        {
+            int sum=0;
+            for(j=i; j<nums.length; j++)
+            {
+                // window/subarray is formed from i=0...j
+                sum = sum + nums[j];
+                max_sum = Math.max(max_sum, sum);
+            }
+            
+        }
+        
+        return max_sum;
+    }
+}

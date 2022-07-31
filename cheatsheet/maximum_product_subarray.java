637b1eaf39981fdee572a7d2479695677a246a0b
Priyanka Salunke <salunkeps99@gmail.com>
2022-07-31 11:38:19 +0530

    Array : added approach2 - DP (linear time)

--- cheatsheet/maximum_product_subarray.java
+++ cheatsheet/maximum_product_subarray.java
@@ -18,3 +18,30 @@ class Solution {
         return max_product;
     }
 }
+
+// Approach-2 : Dynamic programming (DP)
+// Time complexity : O(n) 
+// Link for reference : https://www.youtube.com/watch?v=lXVy6YWFcRM
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        // maintain the min and max of the subarray
+        int local_min = nums[0], local_max = nums[0], max_product = nums[0];
+        
+        for(int i=1; i<nums.length; i++)
+        {
+            // get the maximum value
+            int temp = Math.max(nums[i], Math.max(local_max * nums[i], local_min * nums[i]));
+            // get the minimum value
+            local_min = Math.min(nums[i], Math.min(local_max * nums[i], local_min * nums[i]));
+            
+            // assign the max to local_max
+            local_max = temp;
+            
+            // update the final max_product value
+            max_product = Math.max(max_product, local_max);
+        }
+        
+        return max_product;
+    }
+}

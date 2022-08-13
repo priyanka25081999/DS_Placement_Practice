4902b79621b498c713c441b12a54344e581e9c59
Priyanka Salunke <salunkeps99@gmail.com>
2022-08-13 10:34:10 +0530

    Tree : Validate BST - added optimized approach (Inorder traversal)

--- cheatsheet/validate_BST.java
+++ cheatsheet/validate_BST.java
@@ -55,3 +55,49 @@ class Solution {
         return true;
     }
 }
+
+// Approach 2 - Inorder traversal optimized way
+// Time complexity - O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean flag = true;
+    TreeNode prev = null;
+    
+    public void inorder(TreeNode root)
+    {
+        // base case
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        if(prev!=null && prev.val >= root.val)
+        {
+            flag = false;
+            return;
+        }
+        
+        prev = root;
+        inorder(root.right);
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        inorder(root);
+        return flag;
+    }
+}
+

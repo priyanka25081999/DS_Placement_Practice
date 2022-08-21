47997123fb1d9659665d178d5af81cd0adae22eb
Priyanka Salunke <salunkeps99@gmail.com>
2022-08-21 12:04:05 +0530

    Array : BS - added third approach

--- cheatsheet/search_in_rotated_sorted_array.java
+++ cheatsheet/search_in_rotated_sorted_array.java
@@ -45,3 +45,40 @@ class Solution {
 }
 
 */
+
+/* Third approach - Binary search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        
+        // base condition
+        if(nums[start]>target && nums[end]<target)
+            return -1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;  // to avoid integer overflow
+            
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[start] <= nums[mid])
+            {
+                if(nums[start] <= target && target < nums[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+            else
+            {
+                if(target <= nums[end] && target > nums[mid])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+        }
+        
+        return -1; // no element found
+    }
+}
+*/

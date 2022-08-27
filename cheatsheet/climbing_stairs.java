d56fdc530fba49f9f541f6cf744a5693e4443b74
Priyanka Salunke <salunkeps99@gmail.com>
2022-08-27 17:54:53 +0530

    DP : added bottom-up approach

--- cheatsheet/climbing_stairs.java
+++ cheatsheet/climbing_stairs.java
@@ -20,3 +20,24 @@ class Solution {
         return st.pop();
     }
 }
+
+// DP : bottom-up approach
+// Always remember: last two elements i.e (n-1 and n) are always 1. At the end, you will get the fibonacci series
+// Here in this problem, we require to maintain the last two elements, hence maintaining an array is not required. 
+// We can use two variables
+// Time complexity: O(n) 
+
+class Solution {
+    public int climbStairs(int n) {
+        int one=1, two=1;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            int temp = one;
+            one = one+two;
+            two = temp;
+        }
+        
+        return one;
+    }
+}

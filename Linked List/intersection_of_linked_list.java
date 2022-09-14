d51a42a8cdf9d031186eb4ab379aa16d24c2a79c
Priyanka Salunke <salunkeps99@gmail.com>
2022-09-14 20:41:01 +0530

    LinkedList : Added optimized approach

--- Linked List/intersection_of_linked_list.java
+++ Linked List/intersection_of_linked_list.java
@@ -30,3 +30,27 @@ public class Solution {
         return null;
     }
 }
+
+// Approach-2 : Optimized
+// Take two dummy nodes for each list. Point each to the head of the lists.
+// Iterate over them. If anyone becomes null, point them to the head of the opposite lists and continue iterating until they collide.
+// Time complexity : O(n)
+// Space complexity : O(1)
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA, temp2 = headB;
+        
+        if(headA == null || headB == null)
+            return null;
+        
+        while(temp1 != temp2)
+        {
+            temp1 = temp1 == null ? headB : temp1.next;
+            temp2 = temp2 == null ? headA : temp2.next;
+        }
+        
+        return temp1;
+        
+    }
+}

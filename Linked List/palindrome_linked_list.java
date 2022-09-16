9593ec425920dfb4459d1bb27f58066929db15da
Priyanka Salunke <salunkeps99@gmail.com>
2022-09-16 19:15:31 +0530

    LinkedList : Added another optimal approach

--- Linked List/palindrome_linked_list.java
+++ Linked List/palindrome_linked_list.java
@@ -29,3 +29,59 @@ class Solution {
         return true;
     }
 }
+
+// Approach-2 : reduced space complexity
+// find the middle of linked list
+// reverse the second half of the linkedlist
+// start the pointer from first and middle 
+// compare the elements
+// Time complexity : O(N/2) + O(N/2) + O(N/2) = O(N)
+// O(N/2) for finding the middle element, reversing the list from the middle element, and traversing again to find palindrome respectively.
+// Space complexity : O(1) - Extra data structure is not used
+
+class Solution {
+    
+    // reverse the linked list
+    public ListNode reverse(ListNode curr)
+    {
+        ListNode prev = null, next = null;
+        
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+        // base case
+        if(head==null || head.next==null) 
+            return true;
+        
+        ListNode fast = head, slow = head, temp = head;
+        // find the middle
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        // reverse the linkedlist from middle node
+        slow.next = reverse(slow.next);
+        slow = slow.next;
+        
+        // compare the elements
+        while(slow!=null)
+        {
+            if(temp.val != slow.val)
+                return false;
+            slow = slow.next;
+            temp = temp.next;
+        }
+        return true;
+    }
+}

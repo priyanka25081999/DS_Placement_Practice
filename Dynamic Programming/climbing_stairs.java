dcec256f402237041c30cb2ffded69488a5a6fa8
Priyanka Salunke <salunkeps99@gmail.com>
2022-09-26 12:30:01 +0530

    DP : Added Tabulation approach

--- Dynamic Programming/climbing_stairs.java
+++ Dynamic Programming/climbing_stairs.java
@@ -1,6 +1,30 @@
 // DP : Climbing Stairs
-// Stack approach
 // https://leetcode.com/problems/climbing-stairs/
+
+// Tabulation approach
+// Time complexity : O(N) - We are running a simple iterative loop
+// Space complexity : O(N) - We are using an external array of size ‘n+1’.
+
+class Solution {
+    public int climbStairs(int n) {
+        int arr[] = new int[n+1];
+        Arrays.fill(arr, -1);
+        
+        arr[0]=1;
+        arr[1]=1;
+        
+        for(int i=2; i<=n; i++)
+            arr[i] = arr[i-1]+arr[i-2];
+        
+        return arr[n];
+    }
+}
+
+
+
+
+
+// Stack approach
 
 class Solution {
     public int climbStairs(int n) {

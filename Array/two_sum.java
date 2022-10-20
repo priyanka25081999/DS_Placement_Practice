b64c7691808a0abdb079acb4f366854ef3f9712d
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-20 10:27:28 +0530

    Array : Two sum - Hashmap approach (efficient)

--- Array/two_sum.java
+++ Array/two_sum.java
@@ -78,3 +78,29 @@ class Solution {
         return ans;
     }
 }
+
+// Approach-3 : HashMap - Efficient solution
+// Time complexity : O(N)
+// Space complexity : O(N)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // create a hashmap to store element(nums value) and index pairs
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            // if hashmap contains the target-nums[i] key then 
+            // return the index of target-nums[i] and i
+            if(hmap.containsKey(target-nums[i]))
+                return new int[] { hmap.get(target-nums[i]), i};
+            
+            // if element pair is not exists in the hashmap then add/put it
+            hmap.put(nums[i], i);
+        }
+        
+        // worst case if target sum is not found then return null
+        return null;
+    }
+}
+

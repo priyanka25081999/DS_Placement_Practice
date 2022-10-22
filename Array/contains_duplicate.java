fd6612b33fc5cbf7b32bacaacd024ebb1a250607
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-22 09:06:29 +0530

    Array : Contains duplicate - added more optimized approaches

--- Array/contains_duplicate.java
+++ Array/contains_duplicate.java
@@ -1,5 +1,7 @@
 // Array - Contains Duplicate
 // https://leetcode.com/problems/contains-duplicate/
+// Time Complexity:  O(N*logN), Sorting takes N*logN. Times where N is the length of the array
+// Space Complexity: O(1), Since we are not using any extra space. If we are not counting extra space taken by sorting.
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -9,6 +11,49 @@ class Solution {
         for(int i=1; i<nums.length; i++)
         {
             if(nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+    }
+}
+
+// HashSet approach
+// Time complexity : O(N)
+// Space complexity : O(N) as we are using additional data structure
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // HashSet
+        HashSet<Integer> hset = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            if(hset.contains(nums[i]))
+                return true;
+            hset.add(nums[i]);
+        }
+        return false;        
+    }
+}
+
+// HashMap approach
+// Time complexity : O(N)
+// Space complexity : O(N) as we are using additional data structure
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            hmap.put(nums[i], hmap.getOrDefault(nums[i],0)+1);
+        }
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            int cnt = hmap.get(nums[i]);
+            
+            if(cnt > 1)
                 return true;
         }
         return false;

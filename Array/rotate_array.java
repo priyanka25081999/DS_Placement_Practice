c6f161103062ba3bb93aab777d158d102014d641
Priyanka Salunke <salunkeps99@gmail.com>
2022-11-13 11:34:28 +0530

    Array : Rotate an array k times (right and left rotation)

--- Array/rotate_array.java
+++ Array/rotate_array.java
@@ -60,3 +60,44 @@ class Solution {
         }
     }
 }
+
+// Rotate an array to left by k times
+// Approach : By using partition approach (same as approach-2 above)
+// Time complexity : O(N)
+// Space complexity : O(1), as we are not using temp array. Doing in-place.
+
+class Solution
+{
+    void leftRotate(long nums[], int k,int n)
+    {
+        int mid = k % nums.length;
+        
+        // the below condition is used when mid is non-negative
+        if(mid < 0)
+            mid = mid + nums.length;
+        
+        // from 0 to end. First reverse entire array
+        rotate(nums, 0, n-1);
+        // from 0 to mid-1
+        rotate(nums, 0, nums.length-mid-1);
+        // rotate mid to end
+        rotate(nums, nums.length-mid, nums.length-1);
+        
+    }
+    
+    void rotate(long nums[], int start, int end)
+    {
+        // swap the array elements
+        while(start < end) {
+            long temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+}
+
+// For right rotation -> First, reverse 0 to mid then mid to end and then reverse the entire array.
+// For left rotation -> First, reverse the entire array, then reverse 0 to mid and then reverse mid to end array elements.

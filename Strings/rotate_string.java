75f788b2198e6c7ff3bc3533dd1488f3fb8ec542
Priyanka Salunke <salunkeps99@gmail.com>
2022-11-27 11:12:22 +0530

    String : Rotate string - added the logic with an example

--- Strings/rotate_string.java
+++ Strings/rotate_string.java
@@ -11,3 +11,12 @@ class Solution {
         return (s.length() == goal.length() && (s+s).contains(goal));
     }
 }
+
+/* Idea -
+We can easily see whether it is rotated if goal can be found in (s + s).
+For example, with s = "abcde", goal = "cdeab", we have
+
+“abcdeabcde” (s + s)
+  “cdeab” (goal)
+goal is found in (s + s), so goal is a rotated string of A.
+*/
